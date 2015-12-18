e590e86b738688ccc4cab8d1f8a422066b5987f7
Apple <igor92@bk.ru>
2015-12-18 02:15:51 +0200

    done

---
+++ homework.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 18.12.15
+ * Time: 00:53
+ */
+$name = "Igor";
+$age = 23;
+echo "Menya zovut $name", "<br>";
+echo "Mne $age goda", "<br>";
+$a = 5;
+
+$s = 1/4*$a*sqrt(3);
+echo $s;
+?>
+
